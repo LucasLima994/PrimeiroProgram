5a786c835e25876f4a9f978fdf17a6e9cead5da0
Lucas Lima <lucassilva_.12@hotmail.com>
2022-03-27 23:19:26 -0300

    Second Commit

---
+++ src/dio/com/PrimeiroPrograma.java
@@ -0,0 +1,18 @@
+package dio.com;
+
+import dio.com.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros Livros = new Livros();
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
